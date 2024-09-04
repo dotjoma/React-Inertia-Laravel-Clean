c9283aba96df58c018bb72da51525edeff0c29bb
yikes <bastejoma8@gmail.com>
2024-09-04 13:51:46 +0800

    Initial Commit

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <>
+            <h1>React, Inertia, Laravel</h1>
+            <p>By: Joma</p>
+        </>
+    );
+};
+
+export default Home;
